1ce51b69139dc93141162a310bad450ffa52e094
yasin-arft <yasinarftsocial@gmail.com>
2024-08-15 14:51:48 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Easetone server is running')
+});
+
+app.listen(port, () => {
+  console.log(`Easetone server is running on PORT: ${port}`);
+})
