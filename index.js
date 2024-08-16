59f35ed294ad53c0e84a696dbcc5b254e01eb760
yasin-arft <yasinarftdeveloper@gmail.com>
2024-08-16 14:11:44 +0800

    db connection setup and products get api created

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jzumutc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+// middlewares
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Easetone server is running')
 });
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const productCollection = client.db('easeTone').collection('products');
+
+    app.get('/products', async (req, res) => {
+      const result = await productCollection.find().toArray();
+
+      res.send(result);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Easetone server is running on PORT: ${port}`);
