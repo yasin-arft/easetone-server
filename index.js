40a5e8c76b88799b1f58bbb9c4ba2d0a646f9b69
yasin-arft <yasinarftdeveloper@gmail.com>
2024-08-17 18:33:56 +0800

    deployment

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.get('/', (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const productCollection = client.db('easeTone').collection('products');
